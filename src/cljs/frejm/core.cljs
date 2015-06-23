c720ae0296d871aed883684a1f514678df56737b
Anders Lundström <anders@urkraft.se>
2015-06-23 09:03:24 +0200

    bort med getcourses

--- src/cljs/frejm/core.cljs
+++ src/cljs/frejm/core.cljs
@@ -16,17 +16,8 @@
     (js/Parse.initialize "mWR3FuLC0MB5Q1gm9rtKEfXeKoC6zuk4R7Ds66XG"
                          "0csWP1KiUZEwfZeearVtfp9gfnChIDGBoxozln9P"))
 
-; (defn getcourses []
-;   (let [Course (.extend (.-Object js/Parse) "course")
-;         query  (Query. Course)]
-;     (js/console.log
-;       (.get query "W8uc91QYJL" (clj->js {
-;         "success" (fn [kurs] (js/console.log "Success" kurs.attributes ))
-;         "error"   (fn [obj err] (js/console.log "Error!!"))})))) )
-
 (defn ^:export init []
   (parseinit)
-  ;(getcourses)
   (routes/app-routes)
   (re-frame/dispatch-sync [:initialize-db])
   (mount-root)
