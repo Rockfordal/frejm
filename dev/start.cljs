7bd51cbdb016c8e469501efa78a9284136f854e2
Anders <anders@Bakbord-2.local>
2015-06-29 09:43:33 +0200

    Tror att detta synkar figwheel bättre med den db state vi använder (dock fortfarande problem med att ha vy i annan fil via require)

--- dev/start.cljs
+++ dev/start.cljs
@@ -1,5 +1,6 @@
 (ns rente.start
-  (:require [figwheel.client :as fw]
+  (:require [re-frame.core :as re-frame]
+            [figwheel.client :as fw]
             [rente.client.db :as db]
             [rente.client.app :as app]))
 
@@ -7,6 +8,7 @@
 
 (fw/watch-and-reload
  :websocket-url "ws://localhost:3449/figwheel-ws"
- :jsload-callback #(swap! db/state update-in [:re-render-flip] not))
+ ;:jsload-callback #(swap! db/state update-in [:re-render-flip] not))
+ :jsload-callback #(re-frame/dispatch [:test-flipp]))
 
 (app/main)
