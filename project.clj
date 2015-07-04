0a8dfc2f55974235131a3a94044d03a0c6154f10
Anders Lundström <anders@urkraft.se>
2015-07-04 20:56:08 +0200

    mer optimal minnes.inställningar från Overtone

--- project.clj
+++ project.clj
@@ -1,10 +1,48 @@
+(def JVMOPTS
+  "Per os jvm options. Options common to all cases go under
+  `:any`. Options specific to one OS go under the key returned by
+  `leiningen.core.eval/get-os` for that system. Temporarily disabled
+  options can be kept under `:disabled`."
+  {:any
+   ["-Xms512m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
+    "-XX:+UseParNewGC"            ; Use the new parallel GC in conjunction with
+    "-XX:+UseConcMarkSweepGC"     ;  the concurrent garbage collector
+    "-XX:+CMSConcurrentMTEnabled" ; Enable multi-threaded concurrent gc work (ParNewGC)
+    "-XX:MaxGCPauseMillis=20"     ; Specify a target of 20ms for max gc pauses
+    "-XX:+CMSIncrementalMode"     ; Do many small GC cycles to minimize pauses
+    "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
+    "-XX:NewSize=256m"            ;  generation to be small
+    "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
+                                  ;  improves concurrency by reducing contention on
+                                  ;  the shared heap lock.
+    "-XX:MaxTenuringThreshold=0"] ; Makes the full NewSize available to every NewGC
+                                  ;  cycle, and reduces the pause time by not
+                                  ;  evaluating tenured objects. Technically, this
+                                  ;  setting promotes all live objects to the older
+                                  ;  generation, rather than copying them.
+   :macosx
+   ["-Xdock:name=Frejm"]
+   :disabled
+   ["-XX:ConcGCThreads=2"         ; Use 2 threads with concurrent gc collections
+    "-XX:TieredCompilation"       ; JVM7 - combine both client and server compilation
+                                  ;  strategies
+    "-XX:CompileThreshold=1"      ; JIT each function after one execution
+    "-XX:+PrintGC"                ; Print GC info to stdout
+    "-XX:+PrintGCDetails"         ;  - with details
+    "-XX:+PrintGCTimeStamps"]})   ;  - and timestamps
+
+(defn jvm-opts
+  "Return a complete vector of jvm-opts for the current os."
+  [] (let [os (leiningen.core.utils/get-os)]
+       (vec (set (concat (get JVMOPTS :any)
+                   (get JVMOPTS os)))))) 
+
 (defproject rente "1.0.0"
   :description "A barebones Reagent+Sente app deployable to Heroku. Uses Figwheel (for development build) and Component (lifecycle management)."
   :url "http://enterlab.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.5.1"
-  :jvm-opts ["-Xms256m" "-Xmx384m"]
 
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/clojurescript "0.0-3308"]
@@ -111,4 +149,7 @@
 
   :aliases {"package"
             ["with-profile" "prod" "do"
-             "clean" ["cljsbuild" "once"]]})
+             "clean" ["cljsbuild" "once"]]}
+
+  :jvm-opts ^:replace ~(jvm-opts)
+  )
