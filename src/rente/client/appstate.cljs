96819ab2687f3f91676915efe1b0200eba9c3f76
Anders <anders@Bakbord-2.local>
2015-07-05 22:58:23 +0200

    Ny appstate baserat på todoappen

--- src/rente/client/appstate.cljs
+++ src/rente/client/appstate.cljs
@@ -1,11 +1,21 @@
 (ns rente.client.appstate
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            [cljs.reader]))
 
-(def state (reagent/atom {
-                      :active-panel :home-panel
-                      :messages []
-                      :animals []
-                      :counter 0
+;; -- Default app-db Value  ---------------------------------------------------
+
+(def default-value            ;; what gets put into app-db by default.
+  {:todos   (sorted-map)      ;; use the (int) :id as the key
+   :showing :all
+   :active-panel :home-panel
+   :re-render-flip false
+   }) ;; one of :all :done :completed
+
+;(def state (reagent/atom {
+;                      :active-panel :home-panel
+;                      :messages []
+;                      :animals {}
+;                      :counter 0
 
 ;                      :routes [{:url "/"
 ;                                :panel :home-panel
@@ -14,15 +24,31 @@
 ;                                :panel :demo-panel
 ;                                :label "Rente"}]
 
-                      :doc {} :saved? false
-                      :re-render-flip false}))
+;                      :doc {} :saved? false
+;                      :re-render-flip false}))
 
-(defn set-value! [id value]
-  (swap! state assoc :saved? false)
-  (swap! state assoc-in [:doc id] value))
+;; -- Local Storage  ----------------------------------------------------------
 
-(defn get-value [id]
-  (get-in @state [:doc id]))
+(def lsk "todos-reframe")     ;; local store key
 
-(defn put! [k v]
-  (swap! state assoc k v))
+(defn ls->todos
+  "Read in todos from LS, and process into a form we can merge into app-db."
+  []
+  (some->> (.getItem js/localStorage lsk)
+           (cljs.reader/read-string)
+           (into (sorted-map))         ;; map -> sorted-map
+           (hash-map :todos)))
+
+(defn todos->ls!
+  [todos]
+  (.setItem js/localStorage lsk (str todos)))   ;; sorted-map writen as a map
+
+;(defn set-value! [id value]
+;  (swap! state assoc :saved? false)
+;  (swap! state assoc-in [:doc id] value))
+
+;(defn get-value [id]
+;  (get-in @state [:doc id]))
+
+;(defn put! [k v]
+;  (swap! state assoc k v))
