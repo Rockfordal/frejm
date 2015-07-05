5101705d725582178e887c3aaacaf34b0cb69bf8
Anders <anders@Bakbord-2.local>
2015-07-05 22:58:07 +0200

    subs för todo

--- src/rente/client/subs.cljs
+++ src/rente/client/subs.cljs
@@ -1,24 +1,67 @@
 (ns rente.client.subs
     (:require-macros [reagent.ratom :refer [reaction]])
-    (:require [re-frame.core :as re-frame :refer [register-sub]]))
+    (:require [re-frame.core :refer [register-sub]]))
 
+;; -- Helpers -----------------------------------------------------------------
+
+(defn filter-fn-for
+      [showing-kw]
+      (case showing-kw
+            :active (complement :done)
+            :done   :done
+            :all    identity))
+
+(defn completed-count
+      "return the count of todos for which :done is true"
+      [todos]
+      (count (filter :done (vals todos))))
+
+
+;; -- Subscription handlers and registration  ---------------------------------
 
 (register-sub
- :active-panel
- (fn [db _]
-   (reaction (:active-panel @db))))
+  :todos                ;; usage:  (subscribe [:todos])
+  (fn [db _]
+      (reaction (vals (:todos @db)))))
 
 (register-sub
- :messages
- (fn [db _]
-   (reaction (:messages @db))))
+  :visible-todos
+  (fn [db _]
+      (reaction (let [filter-fn (filter-fn-for (:showing @db))
+                      todos     (vals (:todos @db))]
+                     (filter filter-fn todos)))))
 
 (register-sub
- :animals
- (fn [db _]
-   (reaction (:animals @db))))
+  :completed-count
+  (fn [db _]
+      (reaction (completed-count (:todos @db)))))
 
 (register-sub
- :flippen
- (fn [db]
-   (reaction (:re-render-flip @db))))
+  :footer-stats
+  (fn [db _]
+      (reaction
+        (let [todos (:todos @db)
+              completed-count (completed-count todos)
+              active-count    (- (count todos) completed-count)
+              showing         (:showing @db)]
+             [active-count completed-count showing]))))  ;; tuple
+
+(register-sub
+  :active-panel
+  (fn [db _]
+     (reaction (:active-panel @db))))
+
+;(register-sub
+; :messages
+; (fn [db _]
+;   (reaction (:messages @db))))
+
+;(register-sub
+; :animals
+; (fn [db _]
+;   (reaction (:animals @db))))
+
+;(register-sub
+; :flippen
+; (fn [db]
+;   (reaction (:re-render-flip @db))))
