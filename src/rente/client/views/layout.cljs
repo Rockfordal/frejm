3e2c8600762f96137e2e0ad3503a7f285725eb47
Anders <anders@Bakbord-2.local>
2015-07-05 22:58:44 +0200

    todo till layout

--- src/rente/client/views/layout.cljs
+++ src/rente/client/views/layout.cljs
@@ -4,7 +4,7 @@
 (defn navbar-items [active-panel]
   [:div
     [:li {:class (if (= active-panel "rente-panel")  "active" "")} [:a {:href "#rente"} "Rente"]]
-    [:li {:class (if (= active-panel "test-panel")   "active" "")} [:a {:href "#test"} "Test"]]
+    [:li {:class (if (= active-panel "todo-panel")   "active" "")} [:a {:href "#todo"} "Todo"]]
     [:li {:class (if (= active-panel "parse-panel")   "active" "")} [:a {:href "#parse"} "Parse"]]
     [:li {:class (if (= active-panel "firebase-panel") "active" "")} [:a {:href "#firebase"} "Firebase"]]])
 
