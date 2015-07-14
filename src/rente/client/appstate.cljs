4d80eddf1fe8f4990f6b06b7f77ed1d6b678ed5a
Anders Lundström <anders@urkraft.se>
2015-07-14 14:19:13 +0200

    refaktorisera ut localstate och datascript

--- src/rente/client/appstate.cljs
+++ src/rente/client/appstate.cljs
@@ -1,54 +1,19 @@
-(ns rente.client.appstate
-  (:require [reagent.core :as reagent]
-            [cljs.reader]))
+(ns rente.client.appstate)
 
-;; -- Default app-db Value  ---------------------------------------------------
 
-(def default-value            ;; what gets put into app-db by default.
+(def default-value            ;; -- Default app-db 
   {:todos   (sorted-map)      ;; use the (int) :id as the key
+   :projects []
+   :companies []
+   :animals []
    :showing :all
    :active-panel :home-panel
    :re-render-flip false
-   }) ;; one of :all :done :completed
-
-;(def state (reagent/atom {
-;                      :active-panel :home-panel
-;                      :messages []
-;                      :animals {}
-;                      :counter 0
-
-;                      :routes [{:url "/"
-;                                :panel :home-panel
-;                                :label "Hem"}
-;                                {:url "/demo"
-;                                :panel :demo-panel
-;                                :label "Rente"}]
-
-;                      :doc {} :saved? false
-;                      :re-render-flip false}))
-
-;; -- Local Storage  ----------------------------------------------------------
-
-(def lsk "todos-reframe")     ;; local store key
-
-(defn ls->todos
-  "Read in todos from LS, and process into a form we can merge into app-db."
-  []
-  (some->> (.getItem js/localStorage lsk)
-           (cljs.reader/read-string)
-           (into (sorted-map))         ;; map -> sorted-map
-           (hash-map :todos)))
-
-(defn todos->ls!
-  [todos]
-  (.setItem js/localStorage lsk (str todos)))   ;; sorted-map writen as a map
-
-;(defn set-value! [id value]
-;  (swap! state assoc :saved? false)
-;  (swap! state assoc-in [:doc id] value))
-
-;(defn get-value [id]
-;  (get-in @state [:doc id]))
-
-;(defn put! [k v]
-;  (swap! state assoc k v))
+   :routes [
+            {:url "#rente"    :panel :rente-panel    :label "Rente"}
+            {:url "#todo"     :panel :todo-panel     :label "Att göra"}
+            {:url "#project"  :panel :project-panel  :label "Projekt"}
+            {:url "#company"  :panel :company-panel  :label "Företag"}
+            {:url "#parse"    :panel :parse-panel    :label "Parse"}
+            ;{:url "#firebase" :panel :firebase-panel :label "Firebase"}
+            ]})
---
+++ src/rente/client/ls.cljs
@@ -0,0 +1,31 @@
+(ns rente.client.appstate
+  (:require [reagent.core :as reagent]
+            [cljs.reader]))
+
+;; -- Local Storage  ----------------------------------------------------------
+
+(def lsk "todos-reframe")     ;; local store key
+
+(defn ls->todos
+  "Read in todos from LS, and process into a form we can merge into app-db."
+  []
+  (some->> (.getItem js/localStorage lsk)
+           (cljs.reader/read-string)
+           (into (sorted-map))         ;; map -> sorted-map
+           (hash-map :todos)))
+
+(defn ls->projects []
+  (some->> (.getItem js/localStorage lsk)
+           (cljs.reader/read-string)
+           (into (sorted-map))
+           (hash-map :projects)))
+
+(defn todos->ls!
+  [todos]
+  (.setItem js/localStorage lsk (str todos)))   ;; sorted-map writen as a map
+
+(defn projects->ls! [projects]
+  (js/console.log "tjong: " (str projects))
+  (.setItem js/localStorage lsk (str projects))   ;; sorted-map writen as a map
+  ;projects
+)
