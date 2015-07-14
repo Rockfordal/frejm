d7a19b96a4b74afea4141f4506f8e97f53d646f1
Anders Lundström <anders@urkraft.se>
2015-07-14 19:44:24 +0200

    har lagt funktioner runt firebase tyverr funkar det nog inte nu får fixa sen

--- src/rente/client/views/firebase.cljs
+++ src/rente/client/views/firebase.cljs
@@ -10,15 +10,27 @@
 (defn safe-prn [& msgs]
   (async/put! prn-chan msgs))
 
+(def r (atom nil))
+
+;; --------- anonym login -----------
 ;(m/auth-anon r)
 ;(m/auth-anon r (fn [err auth-data]
 ;                 (safe-prn auth-data (m/auth-info r))))
 ;(m/unauth)
 
-(def r (m/connect "https://innercore.firebaseio.com"))
-(m/auth r "lundstrom123@gmail.com" "tyvärr.." safe-prn)
-(def mikes-friends (m/get-in r [:deep :route]))
+;; --------- vanlig login -----------
+(defn loggain []
+  (let [email "lundstrom123@gmail.com"
+        password "hemligt"
+        conn (m/connect "https://innercore.firebaseio.com")]
+    (m/auth conn email password safe-prn)
+    (reset! r conn)
+    r))
 
+(defn get-mikes-friends []
+  (m/get-in @r [:deep :route]))
+
+;; --------- prenumerera på data -----------
 ;(m/listen-children
 ;  r [:deep]
 ;  (fn [event-type data] (js/console.log "data: " data)))
@@ -32,8 +44,9 @@
 (defn firebase-panel [data]
   (let [messages (subscribe [:messages])
         alert (js/console.log "pang")
-        setkids (m/reset! mikes-friends [{:name "Kid A"} {:name "Kid B"}])
-        getkids (m/deref mikes-friends safe-prn)]
+        ;setkids (m/reset! (get-mikes-friends) [{:name "Kid A"} {:name "Kid B"}])
+        ;getkids (m/deref (get-mikes-friends) safe-prn)
+        ]
     (fn []
       [:div
        [navbar]
@@ -41,7 +54,7 @@
         [:h1 "Firebase"]
         [:br]
         [:p "Messages:" (clj->js @messages)]
-        [:button { :on-click getkids } "get kids"]
-        [:button { :on-click setkids } "reset kids"]
+        ;[:button { :on-click getkids } "get kids"]
+        ;[:button { :on-click setkids } "reset kids"]
         ;[:button { :on-click #(dispatch [:fire-reset (:name "Billy") ])} "Radera"]
         ]])))
