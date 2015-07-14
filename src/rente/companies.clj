001195c17f2b46e7dbbf9ef0d10f66cc53774f26
Anders Lundström <anders@urkraft.se>
2015-07-14 19:45:06 +0200

    företag måste lagras som :company/...

--- src/rente/companies.clj
+++ src/rente/companies.clj
@@ -2,20 +2,16 @@
   (:refer-clojure :exclude [read])
   (:require [rente.db :as db]
             [clj-json.core :as json]))
+   ;[clojure.tools.namespace.repl :refer [refresh refresh-all]]
 
 (defn create!
-  ([]
-     (db/create! {:type :company}))
-  ([m]
-     (db/create! (assoc m :type :company))))
+  ([]  (db/create! {:type :company}))
+  ([m] (db/create! (assoc m :type :company))))
 
 (defn read
-  ([]
-     (db/read :type :company))
-  ([id]
-     (db/read id))
-  ([k v]
-     (db/read k v)))
+  ([]    (db/read :type :company))
+  ([id]  (db/read id))
+  ([k v] (db/read k v)))
 
 (defn update! [id m]
   (db/update! id m))
@@ -31,13 +27,13 @@
 
 (defn init []
   (do
-    (create! {:name    "Baker Tilly"
-              :orgnr   "483938-1134"
-              :phone   "08-42424"})
+    (create! {:company/name    "Baker Tilly"
+              :company/orgnr   "483938-1134"
+              :company/phone   "08-42424"})
 
-    (create! {:name    "Yellow-backed duiker"
-              :orgnr   "483838-2424"
-              :phone   "08-48398"})
+    (create! {:company/name    "Yellow-backed duiker"
+              :company/orgnr   "483838-2424"
+              :company/phone   "08-48398"})
     
      "Skapat företag"
     ))
