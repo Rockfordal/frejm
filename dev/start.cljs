840bc3fd9090e0d7544c08f3d00a7950adec9af2
Anders Lundström <anders@urkraft.se>
2015-08-01 16:36:39 +0200

    figwheel reloadar appen på rätt sätt

--- dev/start.cljs
+++ dev/start.cljs
@@ -1,14 +1,14 @@
 (ns rente.start
-  (:require [reagent.core    :as reagent :refer [force-update-all]]
-            [figwheel.client :as fw]
-            [rente.client.app :as app]))
+  (:require
+    [figwheel.client :as fw]
+    [rente.client.app :as app]))
 
 (enable-console-print!)
 
 (fw/watch-and-reload
  :websocket-url "ws://localhost:3449/figwheel-ws"
- :jsload-callback #(force-update-all))
- ; :jsload-callback mount-root)) ; tydligen ännu bättre 
- ; https://github.com/reagent-project/reagent/issues/112
+ :jsload-callback #(app/render))
+
+(js/console.log "rente.start")
 
 (app/main)
