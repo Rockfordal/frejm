bc42814ee29b69fabccc0a59f11d8b3d1e1b3fc8
Anders Lundström <anders@urkraft.se>
2015-08-02 04:45:33 +0200

    fixat sidenav enkelklick

--- resources/public/init.js
+++ resources/public/init.js
@@ -1,6 +1,10 @@
 (function($){
   $(function(){
-    $(document).on('click', '.button-collapse', function() {
+    $('.button-collapse').sideNav({
+      closeOnClick: true});
+
+    $(document).on('click', '.button-collapse', function(e) {
+        e.preventDefault();
         $('.button-collapse').sideNav({
             closeOnClick: true});
     });
