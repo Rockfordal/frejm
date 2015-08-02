dcd26bf811f39cfeaefa4989c6bcdd6783363646
Anders Lundström <anders@urkraft.se>
2015-08-02 04:48:25 +0200

    routes i separat fil

--- src/rente/client/routes.cljs
+++ src/rente/client/routes.cljs
@@ -1,36 +1,26 @@
 (ns rente.client.routes
-;;     (:require-macros [secretary.core :refer [defroute]])
-;;     (:import goog.History)
-;;     (:require [secretary.core :as secretary]
-;;               [goog.events :as events]
-;;               [goog.history.EventType :as EventType]
-;;               [clojure.string :refer [replace]]
-  ;;               [re-frame.core :as re-frame :refer [dispatch subscribe]])
-  )
+  (:import goog.History)
+  (:require
+    [goog.events :as events]
+    [goog.history.EventType :as EventType]
+    [secretary.core :as secretary :refer-macros [defroute]]
+    [rente.client.state :as state :refer [app-state]]))
 
-;; (defn hook-browser-navigation! []
-;;   (doto (History.)
-;;     (events/listen
-;;      EventType/NAVIGATE
-;;      (fn [event]
-;;        (secretary/dispatch! (.-token event))))
-;;     (.setEnabled true)))
+(defn hook-browser-navigation! []
+  (doto (History.)
+    (events/listen
+     EventType/NAVIGATE
+     (fn [event]
+       (secretary/dispatch! (.-token event))))
+    (.setEnabled true)))
 
-;; (defn app-routes []
-;;   (secretary/set-config! :prefix "#")
+(defn app-routes []
+  (secretary/set-config! :prefix "#")
+  (hook-browser-navigation!))
 
-;;   (defroute "/" []
-;;     (dispatch [:set-active-panel :home-panel]))
+(defroute module-path "/:module" {module :module}
+  (let [module-keys (set (map :key (:modules @app-state)))
+        module-key (or (module-keys (keyword module)) :html2ts)]
+    (swap! app-state
+           assoc :module module-key)))
 
-;;   (defroute "/companies/:id" {:as params}
-;;     ;(println "Företag: " (:id params))
-;;     (dispatch [:set-active-panel :companyedit-panel (:id params)]))
-
-;;   (let [routes (subscribe [:routes])]
-;;     (doseq [route @routes]
-;;       (let [panel   (:panel route)
-;;             hashurl (:url route)
-;;             url     (replace hashurl "#" "/")]
-;;       (defroute [url] [] (dispatch [:set-active-panel panel])))))
-
-;;   (hook-browser-navigation!))
