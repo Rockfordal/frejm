664d03d3cde6f6a0b1f2e98cf51b610057f8f4b4
Anders Lundström <anders@urkraft.se>
2015-08-04 21:57:47 +0200

    remma global funktion som inte används

--- src/rente/client/views/global.cljs
+++ src/rente/client/views/global.cljs
@@ -1,20 +1,19 @@
 (ns rente.client.views.global)
 
-
-(defn input-component [{:keys [title on-save on-stop]}]
-  (let [val (atom title)
-        stop #(do (reset! val "")
-                  (if on-stop (on-stop)))
-        save #(let [v (-> @val str clojure.string/trim)]
-               (if-not (empty? v) (on-save v))
-               (stop))]
-    (fn [props]
-      [:input (merge props
-                     {:type "text"
-                      :value @val
-                      :on-blur save
-                      :on-change #(reset! val (-> % .-target .-value))
-                      :on-key-down #(case (.-which %)
-                                     13 (save)
-                                     27 (stop)
-                                     nil)})])))
+;; (defn input-component [{:keys [title on-save on-stop]}]
+;;   (let [val (atom title)
+;;         stop #(do (reset! val "")
+;;                   (if on-stop (on-stop)))
+;;         save #(let [v (-> @val str clojure.string/trim)]
+;;                (if-not (empty? v) (on-save v))
+;;                (stop))]
+;;     (fn [props]
+;;       [:input (merge props
+;;                      {:type "text"
+;;                       :value @val
+;;                       :on-blur save
+;;                       :on-change #(reset! val (-> % .-target .-value))
+;;                       :on-key-down #(case (.-which %)
+;;                                      13 (save)
+;;                                      27 (stop)
+;;                                      nil)})])))
