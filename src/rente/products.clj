af9564b9362faff59842bd8d6a7e6b1a8b200530
Anders Lundström <anders@urkraft.se>
2015-08-08 13:37:00 +0200

    har städat produkt datomic frågor på servern

--- src/rente/products.clj
+++ src/rente/products.clj
@@ -5,7 +5,7 @@
 
 (def parts [(s/part "frejm")])
 
-(def dbschema                                             ; istället för schema.edn
+(def dbschema          ; istället för schema.edn
   [(s/schema product
     (s/fields
      [name  :string :indexed]
@@ -15,36 +15,27 @@
   ([]  (db/create! {:type :product}))
   ([m] (db/create! (assoc m :type :product))))
 
+(defn find-by-name [name]
+  (d/q '[:find   ?e :in $ ?name
+         :where [?e :product/name ?name]]
+         (db/db) name))
+
+(defn get-by-name [name]
+  (d/touch (d/entity (db/db) (ffirst (find-by-name)))))
+
 (defn getall []
   (map d/touch (db/read :type :product)))
 
-(defn find-product-by-name [name]
-  (d/touch (d/entity (db/db) (ffirst
-    (d/q '[:find ?e
-           :in $
-           :where [?e :product/name ?name]]
-           (db/db) name)))))
-
-(defn find-eid-by-name [product-name]
-  (ffirst (d/q '[:find ?eid
-                 :in $ ?pn
-                 :where [?eid :product/name ?pn]]
-                 (db/db)
-                 project-name)))
-
 (defn setup-db []
-  (d/transact
-    (db/conn)
-   (concat
-    (s/generate-parts parts)
-    (s/generate-schema dbschema))))
+  (d/transact (db/conn)
+    (concat (s/generate-parts parts)
+            (s/generate-schema dbschema))))
 
 (defn init []
-  (do
-    (setup-db)
-    (create! {:product/name    "Filmjölk"
-              :product/price   "49.90"})
+  (do (setup-db)
+      (create! {:product/name    "Filmjölk"
+              :product/price     "49.90"})
 
-    (create! {:product/name    "Prickig Korv"
-              :product/price   "25"})
-     "Skapat produkter"))
+      (create! {:product/name    "Prickig Korv"
+              :product/price     "25"})
+       "Skapat produkter"))
