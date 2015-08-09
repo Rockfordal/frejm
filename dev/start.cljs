f435a5ba226e4e561581845b794239b89b675c51
Anders Lundström <anders@urkraft.se>
2015-08-09 14:14:59 +0200

    toggle-fig är funktionen som figwheel ska köra

--- dev/start.cljs
+++ dev/start.cljs
@@ -1,12 +1,13 @@
 (ns rente.start
   (:require
     [figwheel.client :as fw]
+    [rente.client.util :refer [toggle-fig]]
     [rente.client.app :as app]))
 
 (enable-console-print!)
 
 (fw/watch-and-reload
  :websocket-url "ws://localhost:3449/figwheel-ws"
- :jsload-callback #(app/fig))
+ :jsload-callback #(toggle-fig))
 
 (app/main)
