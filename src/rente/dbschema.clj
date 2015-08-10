ff4bdb56112e28077abe06557e6c3e09fb03baea
Anders Lundström <anders@urkraft.se>
2015-08-10 09:55:37 +0200

    schemat uppdaterat för items

--- src/rente/dbschema.clj
+++ src/rente/dbschema.clj
@@ -5,28 +5,33 @@
 
 (def schema
   [(s/fields
-     [type     :keyword :indexed])
+     [type        :keyword :indexed])
 
-   (s/schema product
-    (s/fields
-     [name     :string :indexed]
-     [price    :string :indexed]))
+;; Sortiment
+;; --------------------------------
+   (s/schema product (s/fields
+     [name        :string :indexed]
+     [price       :string :indexed]))
 
-   (s/schema shelf
-    (s/fields
-     [name     :string :indexed]
-     [location :string :indexed]))
+   (s/schema item (s/fields
+      [quantity   :long :indexed]
+      [shelf      :ref]
+      [product    :ref]))
 
-   (s/schema project
-    (s/fields
+   (s/schema shelf (s/fields
+     [name        :string :indexed]
+     [location    :string :indexed]))
+
+;; Callcenter
+;; --------------------------------
+   (s/schema project (s/fields
      [name        :string :indexed]
      [description :string :indexed]))
 
-   (s/schema company
-    (s/fields
-     [name     :string :indexed]
-     [orgnr    :string :indexed]
-     [phone    :string :indexed]))
+   (s/schema company (s/fields
+     [name        :string :indexed]
+     [orgnr       :string :indexed]
+     [phone       :string :indexed]))
   ])
 
 (defn get-schema []
