cda80fb005a6480258efae331a9bc0acf338382b
Anders Lundström <anders@urkraft.se>
2015-08-14 17:37:07 +0200

    add, delete i 75% klart till company

--- src/rente/client/views/company.cljs
+++ src/rente/client/views/company.cljs
@@ -1,15 +1,12 @@
 (ns rente.client.views.company
   (:require [rum :as r]
+            [datascript :as d]
             [rente.client.dom :as dom :refer [by-id]]
-            [rente.client.state :refer [state get-state]]
+            [rente.client.transactions :as trans]
+            [rente.client.state :refer [state get-state conn]]
 ;             [rente.client.ws :as ws]
 ;             [secretary.core :refer [IRenderRoute render-route encode-query-params]]
 ))
-
-;; (def getcompanies
-;;   (memoize #((ws/get-all :company :companies)
-;;     ;(println "getcompanies")
-;;     nil)))
 
 ;; (defrecord Company [id]
 ;;   IRenderRoute
@@ -19,110 +16,79 @@
 ;;     (str (render-route this) "?"
 ;;          (encode-query-params params))))
 
-;; (defn company-input [{:keys [title on-save on-stop]}]
-;;   (let [val (atom title)
-;;         stop #(do (reset! val "")
-;;                   (if on-stop (on-stop)))
-;;         save #(let [v (-> @val str clojure.string/trim)]
-;;                (if-not (empty? v) (on-save v))
-;;                (stop))]
-;;     (fn [props]
-;;       [:input (merge props
-;;                      {:type "text"
-;;                       :value @val
-;;                       :on-blur save
-;;                       :on-change #(reset! val (-> % .-target .-value))
-;;                       :on-key-down #(case (.-which %)
-;;                                      13 (save)
-;;                                      27 (stop)
-;;                                      nil)})])))
-
-;; ;(def company-edit (with-meta company-input
-;; ;  {:component-did-mount #(.focus (reagent/dom-node %))}))
-
-
-(r/defc company-item [company]
-;;   (let [editing (atom false)]
-    [:tr
-      ;[:td "företag"]
-      [:td (:db/id company)]
-      [:td (:company/name company)]
-      [:td (:company/phone company)]
-;;        [:td
-;;         [:a {:href "/#company" :on-click #(ws/delete (:db/id company) :companies)} [:i.material-icons "delete"]]
-;;         [:a {:href (render-route (Company. (:db/id company)))} [:i.material-icons "view_headline"]]
-;;         ;[:P (:db/id company)]
-         ])
   
-(r/defc company-list [companies]
-  [:table
-   [:thead
+(r/defc company_item [company db]
+  [:tr.company
+   [:td.name  (:company/name  company)]
+   [:td.orgnr (:company/orgnr company)]
+   [:td.name  (:company/phone company)]
+   [:td.email (:company/email company)]
+   [:td
+    [:a {:href "#company" :on-click #(trans/delete (:db/id company) conn)}
+     [:i.material-icons "delete"]]
+    ]])
+; [:a {:href (render-route (Company. (:db/id company)))} [:i.material-icons "view_headline"]]
+
+(r/defc company-list [db]
+ [:table
+  [:thead
     [:tr
-    [:th "Id"]
     [:th "Namn"]
+    [:th "Orgnr"]
     [:th "Telefon"]
-    [:th ""]]
-    [:tbody
-      (for [company companies]
-        ;(company-item company)) ; kommer nog funka sen
-         (r/with-props company-item company :rum/key company))]]])
+    [:th "E-post"]
+    [:th "Åtgärd"]]
+  [:tbody
+    (for [[eid] (sort (d/q '[:find ?e :where [?e :company/name]] db))]
+      (r/with-props company_item (d/entity db eid) db :rum/key [eid])) ; todo: rum react 13 key bug
+    ]]])
 
 (rum/defc company-input [{:keys [id title on-save on-stop]}]
   (let [val (atom title)
         stop #(do (reset! val "")
-                  (if on-stop (on-stop)
-                  ))
+                  (.blur (by-id id))
+                  (if on-stop (on-stop)))
         save #(let [v (-> @val str clojure.string/trim)]
                (if-not (empty? v) (on-save v))
                (stop))]
-;    (fn [props]
-    [:input
-     ;(merge props
-                {:type "text"
+    [:input#icon_telephone.validate
+                {:type "tel"
                 :id id
                 :value @val
-                :on-blur save
+                ;:on-blur save
                 :on-change #(reset! val (-> % .-target .-value))
                 :on-key-down #(case (.-which %)
                                 13 (save)
                                 27 (stop)
-                                nil)}
-   ;  )
-    ]))
-;)
+                                nil)}]))
+
+(r/defc company-field [id icon label]
+  [:div.input-field.col.s6
+    [:i.material-icons.prefix icon]
+   (company-input {:id id
+                   :on-save #(js/console.log "sparar " %)})
+    [:label {:for id} label]])
+
+(r/defc company-form [db]
+ [:form.col_s12
+   ;[:b "Nytt företag"]
+  [:div.row
+   (company-field "company-name"  "person_pin" "Namn")
+   (company-field "company-orgnr" "account_circle" "Orgnr")]
+  [:div.row
+   (company-field "company-phone" "phone" "Telefon")
+   (company-field "company-email" "email" "E-post")]
+
+  [:a.btn.waves-effect.waves-light
+   {:type "submit"
+    :name "action"
+    :on-click #(trans/add)}
+   "adda " [:i.material-icons "send"]]])
 
 (r/defc company_v < rum/reactive [db]
   [:div
-    [:h3 "Företag"]
-      [:div.row
-       (company-list (get-state :companies))
-       [:button.btn {:on-click #(js/alert "yo")} "Lägg till"]
-   ;[:div "Valt Projekt: " (str (get-state :activeproject))]
-       (company-input {:id "new-todo"
-       ;;                 :placeholder "Nytt företag?"
-       ;;                 :on-save #(ws/add-name % :company/name :company :companies)
-                       ;:on-save #(ws/add-company2project % "ica")}]]
-                        :title "Nytt företag"
-                        :on-save #(js/console.log "sparar")
-                        :on-stop #(js/console.log "slut")
-        })
-       [:button.btn {:on-click #(.focus (by-id "new-todo"))} "Fokus"]
-       [:button.btn {:on-click #(.blur (by-id "new-todo"))} "Blur"]
-       ]]) 
-;;    [:div.container
-;;     (getcompanies)
-;;     [:header#header
-;;      [:h2 "Företag"]
-;;        [company-input {:id "new-todo"
-;;                        :placeholder "Nytt företag?"
-;;                        :on-save #(ws/add-name % :company/name :company :companies)}]]
-;;                        ;:on-save #(ws/add-company2project % "ica")}]]
-
-;;     [:div.row
-;;       [company-list companies]
-;;       ;(clj->js (first @companies))
-;;      ]
-;;     ;[:a.btn {:on-click #(println (count @companies))} "antal"]   
-;;     ;[:a.btn {:on-click #(js/alert "tjo")} "Alert"]
-;;     ;[:a.btn {:on-click #(js/console.log "tjo")} "Log"]
-;;     ]]))
+    ;[:h2 "Företag"]
+    [:div.row
+      ;[:div "Valt Projekt: " (str (get-state :activeproject))]
+      (company-list db) [:br]
+      (company-form db)]]) 
