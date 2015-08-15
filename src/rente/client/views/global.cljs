82198eafecd862b96e9a0d806d425bb6bc9d49b1
Anders Lundström <anders@urkraft.se>
2015-08-15 23:58:27 +0200

    global input-component, ikon

--- src/rente/client/views/global.cljs
+++ src/rente/client/views/global.cljs
@@ -1,19 +1,26 @@
-(ns rente.client.views.global)
+(ns rente.client.views.global
+  (:require [rente.client.dom :as dom :refer [by-id]]
+            [rum :as r]))
 
-;; (defn input-component [{:keys [title on-save on-stop]}]
-;;   (let [val (atom title)
-;;         stop #(do (reset! val "")
-;;                   (if on-stop (on-stop)))
-;;         save #(let [v (-> @val str clojure.string/trim)]
-;;                (if-not (empty? v) (on-save v))
-;;                (stop))]
-;;     (fn [props]
-;;       [:input (merge props
-;;                      {:type "text"
-;;                       :value @val
-;;                       :on-blur save
-;;                       :on-change #(reset! val (-> % .-target .-value))
-;;                       :on-key-down #(case (.-which %)
-;;                                      13 (save)
-;;                                      27 (stop)
-;;                                      nil)})])))
+;; Egna materialize funktioner
+(r/defc ikon < rum/static [namn]
+ [:i.material-icons namn])
+
+(rum/defc component-input [{:keys [id title on-save on-stop defval]}]
+  (let [val (atom defval)
+        stop #(do (reset! val "")
+                  (.blur (by-id id))
+                  (if on-stop (on-stop)))
+        save #(let [v (-> @val str clojure.string/trim)]
+               (if-not (empty? v) (on-save v))
+               (stop))]
+    [:input.validate
+                {:type "tel"
+                :id id
+                :value @val
+                ;:on-blur save
+                :on-change #(reset! val (-> % .-target .-value))
+                :on-key-down #(case (.-which %)
+                                13 (save)
+                                27 (stop)
+                                nil)}]))
