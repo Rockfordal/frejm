12617b81689de06c6fc5caec594865d08ac4ecbe
Anders Lundström <anders@urkraft.se>
2015-08-16 13:22:55 +0200

    bort med java-mem-tweaks, whitespace, simple och ändra tillbaka skip-aot på run

--- project.clj
+++ project.clj
@@ -1,44 +1,3 @@
-(def JVMOPTS
-  "Per os jvm options. Options common to all cases go under
-  `:any`. Options specific to one OS go under the key returned by
-  `leiningen.core.eval/get-os` for that system. Temporarily disabled
-  options can be kept under `:disabled`."
-  {:any
-   [
-    ;"-server"                     ; -server = optimized for speed ;  client = optimized for launch time
-    "-Xms512m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
-    "-XX:+UseParNewGC"            ; Use the new parallel GC in conjunction with
-    "-XX:+UseConcMarkSweepGC"     ;  the concurrent garbage collector
-    "-XX:+CMSConcurrentMTEnabled" ; Enable multi-threaded concurrent gc work (ParNewGC)
-    "-XX:MaxGCPauseMillis=20"     ; Specify a target of 20ms for max gc pauses
-    ;"-XX:+CMSIncrementalMode"     ; Do many small GC cycles to minimize pauses  #depecated i senaste jvm
-    "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
-    "-XX:NewSize=256m"            ;  generation to be small
-    "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
-                                  ;  improves concurrency by reducing contention on
-                                  ;  the shared heap lock.
-    "-XX:MaxTenuringThreshold=0"] ; Makes the full NewSize available to every NewGC
-                                  ;  cycle, and reduces the pause time by not
-                                  ;  evaluating tenured objects. Technically, this
-                                  ;  setting promotes all live objects to the older
-                                  ;  generation, rather than copying them.
-   :macosx
-   ["-Xdock:name=Frejm"]
-   :disabled
-   ["-XX:ConcGCThreads=2"         ; Use 2 threads with concurrent gc collections
-    "-XX:TieredCompilation"       ; JVM7 - combine both client and server compilation
-                                  ;  strategies
-    "-XX:CompileThreshold=1"      ; JIT each function after one execution
-    "-XX:+PrintGC"                ; Print GC info to stdout
-    "-XX:+PrintGCDetails"         ;  - with details
-    "-XX:+PrintGCTimeStamps"]})   ;  - and timestamps
-
-(defn jvm-opts
-  "Return a complete vector of jvm-opts for the current os."
-  [] (let [os (leiningen.core.utils/get-os)]
-       (vec (set (concat (get JVMOPTS :any)
-                         (get JVMOPTS os)))))) 
-
 (defproject rente "1.0.0"
   :description "Datomic, Component, Sente, Rum, Datascript"
   :url "http://enterlab.com"
@@ -77,7 +36,7 @@
                  [rum "0.2.7"]
                  [datascript "0.11.5"]
                  [secretary "1.2.3"]
-                 [clj-json "0.5.3"]
+                 ;[clj-json "0.5.3"]
                  [cljs-http "0.1.36"]
                  ;[cljsjs/highlight "8.4-0"]
                  ;[jayq "2.5.4"]
@@ -101,8 +60,8 @@
   :resource-paths ["resources" "resources-index/prod"]
   :target-path "target/%s"
 
-  ;:main ^:skip-aot rente.run
-  :main rente.run
+  :main ^:skip-aot rente.run
+  ;:main rente.run
 
   :cljsbuild
   {:builds
@@ -138,18 +97,6 @@
                                  {:optimizations :none
                                   :source-map true}}}
 
-                    :whitespace {:source-paths ["src/cljs" "test/cljs" "src/brepl"]
-                                :compiler
-                                {:output-to "dev-resources/public/js/whitespace.js"
-                                 :optimizations :whitespace
-                                 :pretty-print true}}
-
-                    :simple {:source-paths ["src/cljs" "test/cljs"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/simple.js"
-                               :optimizations :simple
-                               :pretty-print false}}
-
                     :advanced {:source-paths ["src/cljs" "test/cljs"]
                               :compiler
                               {:output-to "dev-resources/public/js/advanced.js"
@@ -175,5 +122,4 @@
             ["with-profile" "prod" "do"
              "clean" ["cljsbuild" "once"]]}
 
-  :jvm-opts ^:replace ~(jvm-opts)
   )
