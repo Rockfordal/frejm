0f1ed43b8a5655cf6ca5d3d25f1e3c75310864cc
Anders Lundström <anders@urkraft.se>
2015-08-22 13:26:09 +0200

    button, save-button, go-route helpers

--- src/rente/client/views/global.cljs
+++ src/rente/client/views/global.cljs
@@ -6,6 +6,12 @@
 (r/defc ikon < rum/static [namn]
  [:i.material-icons namn])
 
+(r/defc button [opts name icontext]
+  [:a.btn.waves-effect.waves-light opts (str name " ")(ikon icontext)])
+
+(r/defc save-button [save]
+  (button {:on-click save} "Spara " "info"))
+
 (rum/defc component-input [{:keys [id title on-save on-stop defval]}]
   (let [val (atom defval)
         stop #(do (reset! val "")
@@ -24,3 +30,7 @@
                                 13 (save)
                                 27 (stop)
                                 nil)}]))
+
+(defn go-route [this params]
+  (str (render-route this) "?"
+    (encode-query-params params)))
