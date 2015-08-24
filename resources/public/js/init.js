2d56b1c8b4bdaad81c47878660fc2b13d0bceefd
Anders Lundström <anders@urkraft.se>
2015-08-24 08:44:41 +0200

    exempel på hur man gör dropdown, men jag lyckades inte

--- resources/public/js/init.js
+++ resources/public/js/init.js
@@ -10,6 +10,17 @@
         $('.button-collapse').sideNav({
             closeOnClick: true});
     });
+
+    // $('.dropdown-button').dropdown({
+    //     inDuration: 300,
+    //     outDuration: 225,
+    //     constrain_width: false, // Does not change width of dropdown to that of the activator
+    //     hover: true, // Activate on hover
+    //     gutter: 0, // Spacing from edge
+    //     belowOrigin: false // Displays dropdown below the button
+    //     }
+    // );
+
   });
 })(jQuery);
 // Todo: skriva om detta i clojure med tex jayq
