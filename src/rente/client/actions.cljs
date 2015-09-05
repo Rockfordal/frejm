70f5a944fe81743e439b44226606c7ac2ee14a52
Anders Lundström <anders@urkraft.se>
2015-09-05 09:02:28 +0200

    städ

--- src/rente/client/actions.cljs
+++ src/rente/client/actions.cljs
@@ -4,9 +4,9 @@
     [rente.client.state :refer [state conn]]))
 
 
-  (defn add-item []
-    (let [shelfid   (:db/id (:activeshelf @state))
-          productid (:db/id (:activeproduct @state))
-          quantity 5]
-      (println "addar item: " shelfid productid quantity)
-      (trans/add-item conn shelfid productid quantity)))
+(defn add-item []
+  (let [shelfid   (:db/id (:activeshelf @state))
+        productid (:db/id (:activeproduct @state))
+        quantity 5]
+    (println "addar item: " shelfid productid quantity)
+    (trans/add-item conn shelfid productid quantity)))
