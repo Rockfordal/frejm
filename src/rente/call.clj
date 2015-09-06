a6112add53e5160eb390960e03d60f406fcaa04b
Anders Lundström <anders@urkraft.se>
2015-09-06 23:33:26 +0200

    glömde parentes runt conn

--- src/rente/call.clj
+++ src/rente/call.clj
@@ -1,5 +1,7 @@
 (ns rente.call
   (:require [datomic.api :as d]
+            [rente.db :refer [conn]]
+            [rente.dbseed :refer [callcenter]]
             [rente.projects :as projects]
             [rente.companies :as companies]
             [rente.activities :as activities]
@@ -7,7 +9,7 @@
 
 
 (defn seed []
-  (d/transact (conn) rente.dbseed/callcenter))
+  (d/transact (conn) callcenter))
 
 (defn delete-all []
   (activities/delete-all)
